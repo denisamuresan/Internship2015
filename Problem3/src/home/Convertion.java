101f18b18a01a38ef0df77cc2d2d30212d1ca0e6
Denisa Muresan <denisa.muresan@recognos.ro>
2015-08-18 15:34:44 +0300

    Pb3

--- Problem3/src/home/Convertion.java
+++ Problem3/src/home/Convertion.java
@@ -1,51 +1,46 @@
 package home;
-import java.lang.Math;
+
 public class Convertion {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String word = "-23";
+	public static void stringToInt(String word) {
 		int ascii;
 		char lit;
-		int number= 0;
+		int number = 0;
 		int nr = 0;
 		int lung;
 		boolean negativ = false;
-		System.out.println("String: "+word);
+		System.out.println("String: " + word);
 		lung = word.length();
-		
-		ascii = (int)word.charAt(0);
-		if(ascii == 45)
-		{
+
+		if (word.charAt(0) == '-') {
 			negativ = true;
 		}
-		
-		
-		for(int i=0; i<word.length();i++)
-		{
-			
-			if(negativ==true && i!=word.length()-1)
-				i=i+1;
-			lit = word.charAt(i);
-			ascii = (int) lit;
-			
-			
-			
-			if (ascii>47 && ascii<58)
-			{
-				number = number + (int) ((ascii-48) * Math.pow(10,lung-1-i));
-		
-			}
-			/*
-			else if (ascii == 46)
-			{
-				System.out.print(".");
-			}*/
-			if (negativ == true)
-				nr = 0-number;
-		}
-		System.out.print("Number: "+nr);
-		
-	}
 
+		for (int i = 0; i < word.length(); i++) {
+
+			if (negativ == true && i != word.length() - 1) {
+				i = i + 1;
+			}
+			lit = word.charAt(i);
+			ascii = lit;
+
+			if (ascii >= '0' && ascii <= '9') {
+				number = number + (int) ((ascii - 48) * Math.pow(10, lung - 1 - i));
+
+			}
+
+			if (negativ == true) {
+				nr = 0 - number;
+			} else {
+				nr = number;
+			}
+		}
+		System.out.println("Number: " + nr);
+
+	}
+	public static void main(String[] args) {
+		stringToInt("23");
+		stringToInt("-40");
+
+	}
 }
